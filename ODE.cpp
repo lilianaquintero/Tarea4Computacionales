aedf290af43af43068cabde54a6d766a31957ecb
Liliana Quintero <lilianaquintero@MacBook-Air-de-Liliana.local>
2018-11-13 11:00:24 -0500

    Todavia no funcionan las graficas

--- ODE.cpp
+++ ODE.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <cmath>
-float der_x(float vx);
-float der_y(float vy);
-float der_vx(float vx, float v);
-float der_vy(float vy, float v);
-float der_v(float v);
+float der_x(float vxx);
+float der_y(float vyy);
+float der_vx(float vxx, float vv);
+float der_vy(float vyy, float vv);
+float der_v(float vv);
 
 int main()
 {
 	float h=0.0001;
 	int i=2;
-	int puntos=30;
-	float seno=0.8509;
-	float coseno=0.8509;
+	int puntos=10;
+	float seno=0.85;
+	float coseno=0.85;
 	float t[puntos];
 	float x[puntos];
 	float y[puntos];
@@ -22,8 +22,8 @@ int main()
 	t[0] =0;
 	x[0] =0;
 	y[0] =0;
-	v[0] =300;
-	vx[0]=coseno*300;
+	v[0] =300.0;
+	vx[0]=coseno*300.0;
 	vy[0]=seno*300;
 	t[1]= t[0]+h;
 	x[1]= x[0]+h*der_x(vx[0]);
@@ -35,37 +35,37 @@ int main()
 	//std::cout<<t[0]<<" "<<x[0]<<" "<<v[0]<<std::endl;
 	//std::cout<<t[1]<<" "<<x[1]<<" "<<v[1]<<std::endl;
 	//std::cout<<v[1]<<" "<<v[2]<<" "<<vx[0]<<" "<<vy[0]<<std::endl;	
-	while (i<= 30)
+	while (i<=puntos)
 	{
 		t[i]= t[i-1] + h;
-		x[i]= x[i-2] + (h*2*der_x(vx[i-1]));
-		y[i]= y[i-2] + (h*2*der_y(vy[i-1]));
-		vx[i]= vx[i-2] + (h*2*der_vx(vx[i-1], v[i-1]));
-		vy[i]= vy[i-2] + (h*2*der_vy(vy[i-1], v[i-1]));
+		x[i]= x[i-2] + h*2*der_x(vx[i-1]);
+		y[i]= y[i-2] + h*2*der_y(vy[i-1]);
+		vx[i]= vx[i-2] + h*2*der_vx(vx[i-1], v[i-1]);
+		vy[i]= vy[i-2] + h*2*der_vy(vy[i-1], v[i-1]);
 		float bla= (vx[i]*vx[i])+(vy[i]*vy[i]);
 		v[i]=pow(bla,0.5);
 		
-		std::cout<<x[i]<<" "<<y[i]<<std::endl;
+		std::cout<<x[i]<<" "<<y[i]<<" "<<vy[i]<<std::endl;
 		i=i+1;
 	}
 	return 0;
 }
 
-float der_vy(float vy, float v)
+float der_vy(float vyy, float vv)
 {
-	return -10.0-(abs(v)*vy);
+	return -10.0-(abs(vv)*vyy);
 }
 
-float der_vx(float vx, float v)
+float der_vx(float vxx, float vv)
 {
-	return -vx*abs(v);
+	return -vxx*abs(vv);
 }
 
-float der_x(float vx)
+float der_x(float vxx)
 {
-	return vx;
+	return vxx;
 }
-float der_y(float vy)
+float der_y(float vyy)
 {
-	return vy;
+	return vyy;
 }
