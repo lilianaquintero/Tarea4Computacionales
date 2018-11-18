a930334858ee735cd647eeb81750c24a8352f185
Liliana Quintero <lilianaquintero@MacBook-Air-de-Liliana.local>
2018-11-18 12:08:15 -0500

    PDE sin periodicas

--- PDE.cpp
+++ PDE.cpp
@@ -46,4 +46,91 @@ int main()
 	float presente[50][50];
 	int k=0;
 	t= t+delta_t;
+	/*while(t<=t_max)
+	{
+		for (int i=0; i<50; i++)
+		{
+			for(int j=0; j<50;j++)
+			{				
+								
+				presente[i][j]=((nu*delta_t*(pasado[i+1][j]-2*pasado[i][j]+pasado[i-1][j]))+(nu*delta_t*(pasado[i][j+1]-2*pasado[i][j]+pasado[i][j-1])))+pasado[i][j];
+				presente[0][j]=10;
+				presente[49][j]=10;
+				presente[i][0]=10;
+				presente[i][49]=10;
+
+				if(   (pow(i-25,2) + pow(j-25,2)) <=25)				
+				{
+					presente[i][j]= 100;
+				}			
+				
+			}
+		}
+		for (int k=0; k<50; k++)
+		{
+			for (int l=0; l<50; l++)
+			{
+				pasado[k][l]=presente[k][l];
+			}
+		}
+		k=k+1;
+		t=t+delta_t;
+	}
+	ofstream myfile;
+	myfile.open ("datos_fijo.dat");
+	for (int k=0; k<50; k++)
+	{
+		for (int l=0; l<50; l++)
+		{
+					
+			myfile<<presente[k][l]<<" ";
+		}
+		myfile<< std::endl;
+	}
+	myfile.close();*/
+	t= 0+delta_t;
+	float t_max2=10;
+	while(t<=t_max2)
+	{
+		for (int i=0; i<50; i++)
+		{
+			for(int j=0; j<50;j++)
+			{				
+								
+				presente[i][j]=((nu*delta_t*(pasado[i+1][j]-2*pasado[i][j]+pasado[i-1][j]))+(nu*delta_t*(pasado[i][j+1]-2*pasado[i][j]+pasado[i][j-1])))+pasado[i][j];
+				presente[0][j]=pasado[1][j];
+				presente[49][j]=pasado[48][j];
+				presente[i][0]=pasado[i][1];
+				presente[i][49]=pasado[i][48];
+
+				if(   (pow(i-25,2) + pow(j-25,2)) <=25)				
+				{
+					presente[i][j]= 100;
+				}			
+				
+			}
+		}
+		for (int k=0; k<50; k++)
+		{
+			for (int l=0; l<50; l++)
+			{
+				pasado[k][l]=presente[k][l];
+			}
+		}
+		k=k+1;
+		t=t+delta_t;
+	}
+	ofstream myfile2;
+	myfile2.open ("datos_abierto.dat");
+	for (int k=0; k<50; k++)
+	{
+		for (int l=0; l<50; l++)
+		{
+					
+			myfile2<<presente[k][l]<<" ";
+		}
+		myfile2<< std::endl;
+	}
+	myfile2.close();
+
 	
