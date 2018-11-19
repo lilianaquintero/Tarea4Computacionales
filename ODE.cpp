45c1c04e8563a94f22d95536f15841bc4f1c8592
Liliana Quintero <lilianaquintero@MacBook-Air-de-Liliana.local>
2018-11-18 19:26:08 -0500

    ODEs runge kutta falta 30 grados en adelante

--- ODE.cpp
+++ ODE.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <cmath>
+#include<fstream>
+using namespace std;
+
 float der_x(float vxx);
 float der_y(float vyy);
-float der_vx(float vxx, float vv);
-float der_vy(float vyy, float vv);
+float der_vx(float vyy, float vxx);
+float der_vy(float vyy, float vxx);
 float der_v(float vv);
 
 int main()
 {
 	float h=0.0001;
 	//int i=2;
-	int puntos=1000;
-	float seno=0.85;
-	float coseno=0.85;
+	int puntos=16000;
+	float seno=0.70;
+	float coseno=0.70;
 	float t[puntos];
 	float x[puntos];
 	float y[puntos];
@@ -24,40 +27,235 @@ int main()
 	y[0] =0;
 	v[0] =300.0;
 	vx[0]=coseno*300.0;
-	vy[0]=seno*300;
-	t[1]= t[0]+h;
-	x[1]= x[0]+h*der_x(vx[0]);
-	y[1]= y[0]+h*der_y(vy[0]);
-	vx[1]=vx[0]+h*der_vx(vx[0], v[0]);
-	vy[1]=vy[0]+h*der_vy(vy[0], v[0]);
-	float bla= (vx[1]*vx[1])+(vy[1]*vy[1]);
-	v[1]=pow(bla,0.5);
-	//std::cout<<t[0]<<" "<<x[0]<<" "<<v[0]<<std::endl;
-	//std::cout<<t[1]<<" "<<x[1]<<" "<<v[1]<<std::endl;
-	//std::cout<<v[1]<<" "<<v[2]<<" "<<vx[0]<<" "<<vy[0]<<std::endl;	
-	for (int i =2; i<puntos; i++)
+	vy[0]=seno*300;	
+	ofstream myfile;
+	myfile.open("datos45.dat");
+	for (int i =1; i<puntos; i++)
 	{
 		t[i]= t[i-1] + h;
-		x[i]= x[i-2] + h*2*der_x(vx[i-1]);
-		y[i]= y[i-2] + h*2*der_y(vy[i-1]);
-		vx[i]= vx[i-2] + h*2*der_vx(vx[i-1], v[i-1]);
-		vy[i]= vy[i-2] + h*2*der_vy(vy[i-1], v[i-1]);
+		float k1, k2, k3, k4, l1,l2,l3,l4,m1,m2,m3,m4,n1,n2,n3,n4;
+		k1=h* der_x(vx[i-1]);
+		k2=h*der_x(vx[i-1]+(0.5*k1));
+		k3=h*der_x(vx[i-1]+(0.5*k2));
+		k4=h*der_x(vx[i-1] +k3);
+		
+		
+		l1=h* der_y(vy[i-1]);
+		l2=h*der_y(vy[i-1]+ (0.5*l1));
+		l3=h*der_y(vy[i-1]+(0.5*l2));
+		l4=h*der_y(vy[i-1] +l3);
+
+		
+		m1=h* der_vx(vx[i-1], v[i-1]);
+		n1=h* der_vy(vy[i-1], v[i-1]);
+
+		m2=h*der_vx(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+		n2=h*der_vy(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+
+		m3=h*der_vx(vy[i-1]+0.5*n2, vx[i-1]+(0.5*m2));
+		n3=h*der_vy(vy[i-1]+(0.5*n2), vx[i-1]+(0.5*m2));
+
+		m4=h*der_vx(vy[i-1] +n3, vx[i-1]+m3);
+		n4=h*der_vy(vy[i-1] +n3, vx[i-1]+m3);
+
+		float a_k, a_l, a_m, a_n;
+		a_k=(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
+		a_l=(1.0/6.0)*(l1+2.0*l2+2.0*l3+l4);
+		a_m=(1.0/6.0)*(m1+2.0*m2+2.0*m3+m4);
+		a_n=(1.0/6.0)*(n1+2.0*n2+2.0*n3+n4);
+
+		x[i]= x[i-1] + a_k;
+		y[i]= y[i-1] + a_l;
+		vx[i]= vx[i-1] + a_m;
+		vy[i]= vy[i-1] + a_n;
 		float bla= (vx[i]*vx[i])+(vy[i]*vy[i]);
 		v[i]=pow(bla,0.5);
 		
-		std::cout<<x[i]<<" "<<y[i]<<" "<<vy[i]<<std::endl;
+		myfile<<x[i]<<" "<<y[i]<<" "<<std::endl;
 	}
+	myfile.close();
+	puntos=15000;
+	coseno=0.9848;
+	seno=0.1736;
+	t[0] =0;
+	x[0] =0;
+	y[0] =0;
+	v[0] =300.0;
+	vx[0]=coseno*300.0;
+	vy[0]=seno*300;
+	ofstream myfile2;
+	myfile2.open("datos10.dat")	
+	for (int i =1; i<puntos; i++)
+	{
+		t[i]= t[i-1] + h;
+		float k1, k2, k3, k4, l1,l2,l3,l4,m1,m2,m3,m4,n1,n2,n3,n4;
+		k1=h* der_x(vx[i-1]);
+		k2=h*der_x(vx[i-1]+(0.5*k1));
+		k3=h*der_x(vx[i-1]+(0.5*k2));
+		k4=h*der_x(vx[i-1] +k3);
+		
+		
+		l1=h* der_y(vy[i-1]);
+		l2=h*der_y(vy[i-1]+ (0.5*l1));
+		l3=h*der_y(vy[i-1]+(0.5*l2));
+		l4=h*der_y(vy[i-1] +l3);
+
+		
+		m1=h* der_vx(vx[i-1], v[i-1]);
+		n1=h* der_vy(vy[i-1], v[i-1]);
+
+		m2=h*der_vx(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+		n2=h*der_vy(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+
+		m3=h*der_vx(vy[i-1]+0.5*n2, vx[i-1]+(0.5*m2));
+		n3=h*der_vy(vy[i-1]+(0.5*n2), vx[i-1]+(0.5*m2));
+
+		m4=h*der_vx(vy[i-1] +n3, vx[i-1]+m3);
+		n4=h*der_vy(vy[i-1] +n3, vx[i-1]+m3);
+
+		float a_k, a_l, a_m, a_n;
+		a_k=(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
+		a_l=(1.0/6.0)*(l1+2.0*l2+2.0*l3+l4);
+		a_m=(1.0/6.0)*(m1+2.0*m2+2.0*m3+m4);
+		a_n=(1.0/6.0)*(n1+2.0*n2+2.0*n3+n4);
+
+		x[i]= x[i-1] + a_k;
+		y[i]= y[i-1] + a_l;
+		vx[i]= vx[i-1] + a_m;
+		vy[i]= vy[i-1] + a_n;
+		float bla= (vx[i]*vx[i])+(vy[i]*vy[i]);
+		v[i]=pow(bla,0.5);
+		
+		myfile2<<x[i]<<" "<<y[i]<<" "<<std::endl;
+	}
+	myfile2.close();
+
+	puntos=15000;
+	coseno=0.93969262;
+	seno=0.34202014;
+	t[0] =0;
+	x[0] =0;
+	y[0] =0;
+	v[0] =300.0;
+	vx[0]=coseno*300.0;
+	vy[0]=seno*300;
+	ofstream myfile2;
+	myfile3.open("datos20.dat")	
+	for (int i =1; i<puntos; i++)
+	{
+		t[i]= t[i-1] + h;
+		float k1, k2, k3, k4, l1,l2,l3,l4,m1,m2,m3,m4,n1,n2,n3,n4;
+		k1=h* der_x(vx[i-1]);
+		k2=h*der_x(vx[i-1]+(0.5*k1));
+		k3=h*der_x(vx[i-1]+(0.5*k2));
+		k4=h*der_x(vx[i-1] +k3);
+		
+		
+		l1=h* der_y(vy[i-1]);
+		l2=h*der_y(vy[i-1]+ (0.5*l1));
+		l3=h*der_y(vy[i-1]+(0.5*l2));
+		l4=h*der_y(vy[i-1] +l3);
+
+		
+		m1=h* der_vx(vx[i-1], v[i-1]);
+		n1=h* der_vy(vy[i-1], v[i-1]);
+
+		m2=h*der_vx(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+		n2=h*der_vy(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+
+		m3=h*der_vx(vy[i-1]+0.5*n2, vx[i-1]+(0.5*m2));
+		n3=h*der_vy(vy[i-1]+(0.5*n2), vx[i-1]+(0.5*m2));
+
+		m4=h*der_vx(vy[i-1] +n3, vx[i-1]+m3);
+		n4=h*der_vy(vy[i-1] +n3, vx[i-1]+m3);
+
+		float a_k, a_l, a_m, a_n;
+		a_k=(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
+		a_l=(1.0/6.0)*(l1+2.0*l2+2.0*l3+l4);
+		a_m=(1.0/6.0)*(m1+2.0*m2+2.0*m3+m4);
+		a_n=(1.0/6.0)*(n1+2.0*n2+2.0*n3+n4);
+
+		x[i]= x[i-1] + a_k;
+		y[i]= y[i-1] + a_l;
+		vx[i]= vx[i-1] + a_m;
+		vy[i]= vy[i-1] + a_n;
+		float bla= (vx[i]*vx[i])+(vy[i]*vy[i]);
+		v[i]=pow(bla,0.5);
+		
+		myfile3<<x[i]<<" "<<y[i]<<" "<<std::endl;
+	}
+	myfile3.close();
+
+	puntos=15000;
+	coseno=0.8660254;
+	seno=0.5;
+	t[0] =0;
+	x[0] =0;
+	y[0] =0;
+	v[0] =300.0;
+	vx[0]=coseno*300.0;
+	vy[0]=seno*300;
+	ofstream myfile2;
+	myfile4.open("datos30.dat")	
+	for (int i =1; i<puntos; i++)
+	{
+		t[i]= t[i-1] + h;
+		float k1, k2, k3, k4, l1,l2,l3,l4,m1,m2,m3,m4,n1,n2,n3,n4;
+		k1=h* der_x(vx[i-1]);
+		k2=h*der_x(vx[i-1]+(0.5*k1));
+		k3=h*der_x(vx[i-1]+(0.5*k2));
+		k4=h*der_x(vx[i-1] +k3);
+		
+		
+		l1=h* der_y(vy[i-1]);
+		l2=h*der_y(vy[i-1]+ (0.5*l1));
+		l3=h*der_y(vy[i-1]+(0.5*l2));
+		l4=h*der_y(vy[i-1] +l3);
+
+		
+		m1=h* der_vx(vx[i-1], v[i-1]);
+		n1=h* der_vy(vy[i-1], v[i-1]);
+
+		m2=h*der_vx(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+		n2=h*der_vy(vy[i-1]+ (0.5*n1), vx[i-1]+(0.5*m1));
+
+		m3=h*der_vx(vy[i-1]+0.5*n2, vx[i-1]+(0.5*m2));
+		n3=h*der_vy(vy[i-1]+(0.5*n2), vx[i-1]+(0.5*m2));
+
+		m4=h*der_vx(vy[i-1] +n3, vx[i-1]+m3);
+		n4=h*der_vy(vy[i-1] +n3, vx[i-1]+m3);
+
+		float a_k, a_l, a_m, a_n;
+		a_k=(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
+		a_l=(1.0/6.0)*(l1+2.0*l2+2.0*l3+l4);
+		a_m=(1.0/6.0)*(m1+2.0*m2+2.0*m3+m4);
+		a_n=(1.0/6.0)*(n1+2.0*n2+2.0*n3+n4);
+
+		x[i]= x[i-1] + a_k;
+		y[i]= y[i-1] + a_l;
+		vx[i]= vx[i-1] + a_m;
+		vy[i]= vy[i-1] + a_n;
+		float bla= (vx[i]*vx[i])+(vy[i]*vy[i]);
+		v[i]=pow(bla,0.5);
+		
+		myfile4<<x[i]<<" "<<y[i]<<" "<<std::endl;
+	}
+	myfile4.close();
 	return 0;
 }
 
-float der_vy(float vyy, float vv)
+float der_vy(float vyy, float vxx)
 {
-	return -10.0-(abs(vv)*vyy);
+	float bla= (vxx*vxx)+(vyy*vyy);
+	float vv=pow(bla,0.5);
+	return -(10.0+(vv*vyy));
 }
 
-float der_vx(float vxx, float vv)
+float der_vx(float vyy, float vxx)
 {
-	return -10.0-vxx*abs(vv);
+	float bla= (vxx*vxx)+(vyy*vyy);
+	float vv=pow(bla,0.5);	
+	return -vxx*abs(vv);
 }
 
 float der_x(float vxx)
